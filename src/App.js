ff224c68965b1dde439a5b17eae752dd2bae0989
Jack Carrick <jackcarrick@Jacks-MacBook-Pro.local>
2021-02-08 22:00:55 -0800

    added tone sounds

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
 import React from "react";
-import * as Tone from 'tone'
+import * as Tone from "tone";
 class App extends React.Component {
   constructor(props) {
-    super(props)
-    const transport = Tone.Transport
-    const clicker = time => {
-      console.log(time)
-      randomString = getRandomString()
-    }
+    super(props);
+    const transport = Tone.Transport;
 
-    const getRandomString = () => `Random String ${Math.floor(Math.random() * 1000000)}`
-    let randomString = getRandomString()
-    const loop = new Tone.Loop(clicker, 1)
+    const synth = new Tone.MonoSynth().toDestination();
+
+    const clicker = (time) => {
+      console.log(time);
+      synth.triggerAttackRelease(
+        Math.floor(Math.random() * 440) + 200,
+        0.3,
+        time
+      );
+      randomString = getRandomString();
+    };
+
+    const getRandomString = () =>
+      `Random String ${Math.floor(Math.random() * 1000000)}`;
+    let randomString = getRandomString();
+    const loop = new Tone.Loop(clicker, 0.2);
     const startLoop = () => {
-      transport.start()
-      loop.start(0)
-      transport.start()
-    }
+      transport.start();
+      loop.start(0);
+      transport.start();
+    };
 
     this.state = {
       startLoop,
-      randomString
-    }
+      randomString,
+    };
   }
 
   render() {
-
-    return (<button onClick={this.state.startLoop}> {this.state.randomString} </button>)
+    return (
+      <button onClick={this.state.startLoop}>
+        {" "}
+        {this.state.randomString}{" "}
+      </button>
+    );
   }
-
 }
 
-
-export default App
+export default App;
